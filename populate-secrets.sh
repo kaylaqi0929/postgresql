9bcb00822812aa4c24e4989b7c2e5ae031e21654
Johann Queuniet <sub_code.git@queuniet.fr>
2019-10-23 23:30:45 +0200

    Backup the database to Backblaze

--- populate-secrets.sh
+++ populate-secrets.sh
@@ -2,4 +2,10 @@
 
 work_dir=$(dirname $0)
 
-echo 'POSTGRES_PASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) >>$work_dir/secrets.env
+echo 'POSTGRES_PASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) >$work_dir/secrets.env
+
+echo 'B2_KEY_ID='$(pass perso/Infra/backblaze.com/backups_sgbd/keyid) >$work_dir/backup-postgres.env
+echo 'B2_APPLICATION_KEY='$(pass perso/Infra/backblaze.com/backups_sgbd/secret) >>$work_dir/backup-postgres.env
+echo 'PGPASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) >>$work_dir/backup-postgres.env
+echo 'PGHOST=postgres12' >>$work_dir/backup-postgres.env
+echo 'PGUSER=postgres' >>$work_dir/backup-postgres.env
