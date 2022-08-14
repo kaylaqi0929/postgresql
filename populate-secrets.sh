c6f00ad336b85ab7f70159088e1ebce2a50c245b
Johann Queuniet <sub_code.git@queuniet.fr>
2022-08-14 15:33:00 +0200

    Update to PostgreSQL 14

--- populate-secrets.sh
+++ populate-secrets.sh
@@ -7,5 +7,5 @@ echo 'POSTGRES_PASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) 
 echo 'B2_KEY_ID='$(pass perso/Infra/backblaze.com/backups_sgbd/keyid) >$work_dir/backup-postgres.env
 echo 'B2_APPLICATION_KEY='$(pass perso/Infra/backblaze.com/backups_sgbd/secret) >>$work_dir/backup-postgres.env
 echo 'PGPASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) >>$work_dir/backup-postgres.env
-echo 'PGHOST=postgres12' >>$work_dir/backup-postgres.env
+echo 'PGHOST=postgres14' >>$work_dir/backup-postgres.env
 echo 'PGUSER=postgres' >>$work_dir/backup-postgres.env
