cc027ab900435d6e989f6937d5d1abd1334f607c
Johann Queuniet <sub_code.git@queuniet.fr>
2022-09-11 12:38:14 +0200

    Update cronjob for S3 backups

--- populate-secrets.sh
+++ populate-secrets.sh
@@ -4,8 +4,10 @@ work_dir=$(dirname $0)
 
 echo 'POSTGRES_PASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) >$work_dir/secrets.env
 
-echo 'B2_KEY_ID='$(pass perso/Infra/backblaze.com/backups_sgbd/keyid) >$work_dir/backup-postgres.env
-echo 'B2_APPLICATION_KEY='$(pass perso/Infra/backblaze.com/backups_sgbd/secret) >>$work_dir/backup-postgres.env
+echo 'HOSTNAME=k8s_hetzner' >$work_dir/backup-postgres.env
+echo 'S3_ENDPOINT=https://s3.us-west-002.backblazeb2.com' >>$work_dir/backup-postgres.env
+echo 'S3_ACCESS_KEY_ID='$(pass perso/Infra/backblaze.com/backups_sgbd/keyid) >>$work_dir/backup-postgres.env
+echo 'S3_SECRET_ACCESS_KEY='$(pass perso/Infra/backblaze.com/backups_sgbd/secret) >>$work_dir/backup-postgres.env
 echo 'PGPASSWORD='$(pass perso/Servers/PostgreSQL/kube-lordran/postgres) >>$work_dir/backup-postgres.env
 echo 'PGHOST=postgres14' >>$work_dir/backup-postgres.env
 echo 'PGUSER=postgres' >>$work_dir/backup-postgres.env
